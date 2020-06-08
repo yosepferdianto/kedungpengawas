3dd7c8bf96536da54df3559cb9debe35bded8bee
YosepFDN <yosep.fdn@gmail.com>
2020-06-08 14:42:08 +0700

    button masuk form ubah berita

--- application/controllers/admin/Berita.php
+++ application/controllers/admin/Berita.php
@@ -47,7 +47,7 @@ class Berita extends CI_Controller {
       $row[]   = longdate_indo($tanggal).'<br>'.$waktu.' WIB<hr class="style1-ms-0">'.'Pembuat : @'.$ls->username.'<br>('.$ls->nama_lengkap.')';
       //add html for action
       $row[] = '<div class="text-center">
-        <a class="btn btn-primary btn-sm" title="Ubah" onclick="edit(' . "'" . $ls->id_berita . "'" . ')"><i class="fa fa-edit"></i> Ubah</a>
+        <a class="btn btn-primary btn-sm" title="Ubah" href="'.base_url('admin/berita/ubah_berita/'.$ls->id_berita).'"><i class="fa fa-edit"></i> Ubah</a>
         <a class="btn btn-default btn-sm text-red" title="Hapus" onclick="hapus(' . "'" . $ls->id_berita . "'" . ')"><i class="fa fa-trash"></i> Hapus</a>
       </div>';
 
@@ -91,9 +91,9 @@ class Berita extends CI_Controller {
     $url = $this->config->item('upload_url') . "./storage/upload/berita/";
     $rawFileName = $_FILES['file']['name'];
     $fileName = str_replace(' ', '-', $rawFileName);
-    $formatfile	= date('YmdHis') . $fileName;
+    $formatfile	= $fileName;
     if (!empty($rawFileName)){
-      $pathfileName	= $url . date('YmdHis') . $fileName;
+      $pathfileName	= $url . $fileName;
     } else {
       $pathfileName	= "./storage/img/noimage.png";
     }
@@ -134,6 +134,125 @@ class Berita extends CI_Controller {
         $this->data_berita->save($dataBerita);
         $status = TRUE;
         $msg   = "Proses membuat berita berhasil";
+			}
+    }
+    echo json_encode(array('status' => $status, 'message' => $msg));
+  }
+
+  public function load_data_berita($id) {
+    $status = FALSE;
+    $msg    = 'Memuat data berita...';
+
+    $getData = $this->data_berita->getDataBerita($id);
+
+    foreach($getData as $p) {
+      $create_date = date("Y-m-d", strtotime($p->created_at));
+      $create_time = date("H:i", strtotime($p->created_at));
+      $update_date = date("Y-m-d", strtotime($p->updated_at));
+      $update_time = date("H:i", strtotime($p->updated_at));
+      $data = [
+        // Created_at detail
+        'create_date' => longdate_indo($create_date),
+        'create_time' => $create_time,
+        // Updated_at detail
+        'update_date' => longdate_indo($update_date),
+        'update_time' => $update_time,
+        // Author detail
+        'author' => ucwords($p->username),
+        // Content ------------------------------------
+        'judul_berita' => $p->judul_berita,
+        'sub_judul' => $p->sub_judul,
+        'isi_berita' => $p->isi_berita,
+        'foto' => $p->foto,
+        // Priority
+        'prioritas' => $p->prioritas,
+      ];
+    }
+
+    if(empty($data)) {
+      $status = FALSE;
+      $msg    = 'Gagal memuat data berita!';
+    } else {
+      $status = TRUE;
+      $msg    = 'Memuat data berita berhasil';
+    }
+
+    echo json_encode(array('dataForm' => $data, 'status' => $status, 'message' => $msg));
+    
+  }
+
+  public function ubah_berita($id) {
+    $data['title'] = "Desa Kedung Pengawas";
+    $data['id'] = $id;
+    $url = $this->uri->segment(1);
+
+    if ($this->session->userdata('staff_desa_in') && $url == 'admin') {
+      $this->template->admin_page('admin_page/edit_berita', $data);
+		} else {
+      redirect('/staffadmin');
+    }
+  }
+
+  public function ubahberita() {
+    $status   = FALSE;
+    $msg     = "(EXP)";
+
+    $idAdmin = $this->session->userdata('id_akun_admin');
+
+    $cekPrioritas = $this->data_berita->cek_prioritas();
+    $prioritas  = $cekPrioritas + 1;
+
+    $id_berita   = $this->input->post_get('idBerita');
+    $judul      = $this->input->post_get('judul');
+    $sub_judul  = $this->input->post_get('sub_judul');
+    $isi_berita  = $this->input->post_get('isi_berita');
+    $url_img  = $this->input->post_get('set_img');
+
+    $url = $this->config->item('upload_url') . "./storage/upload/berita/";
+    $rawFileName = $_FILES['file']['name'];
+    $fileName = str_replace(' ', '-', $rawFileName);
+    $formatfile	= $fileName;
+    if (!empty($rawFileName)){
+      $pathfileName	= $url . $fileName;
+    } else {
+      $pathfileName	= $url_img;
+    }
+    $directory = $this->config->item('upload_destination') .  "./storage/upload/berita/";
+
+    if ($this->session->userdata('staff_desa_in') == "") {
+      $status = FALSE;
+      $msg   = "error_login_session";
+    } else if (empty($idAdmin)) {
+      $status = FALSE;
+      $msg   = "id_admin_null";
+    } else if (empty($judul)) {
+      $status = FALSE;
+      $msg   = "judul_null";
+    } else if (empty($sub_judul)) {
+      $status = FALSE;
+      $msg   = "sub_judul_null";
+		} else if (empty($isi_berita)) {
+      $status = FALSE;
+      $msg   = "isi_berita_null";
+    } else {
+
+      $dataBerita = [
+        'id_akun_admin'   => $idAdmin,
+        'prioritas'       => $prioritas,
+        'judul_berita'    => $judul,
+        'sub_judul'       => $sub_judul,
+        'isi_berita'      => $isi_berita, 
+        'foto'            => $pathfileName,
+      ];
+      
+      if (empty($dataBerita)) {
+				$status = FALSE;
+				$msg 	= "error_input_array";
+			} else {
+        move_uploaded_file($_FILES["file"]["tmp_name"], trim($directory . $formatfile));
+        $this->data_berita->update_berita(['id_berita' => $id_berita], $dataBerita);
+        $status = TRUE;
+        $msg   = "Proses mengubah berita berhasil";
 			}
     }
     echo json_encode(array('status' => $status, 'message' => $msg));
